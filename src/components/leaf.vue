3364de30924f11d57d64f66cf4510589eb63cca9
genyu818 <wanghengyu1028@gmail.com>
2020-06-10 11:34:53 +0200

    case study leaf dataset

--- src/components/leaf.vue
+++ src/components/leaf.vue
@@ -3,22 +3,20 @@
     <div id="table-wrapper">
       <table>
         <tr>
-          <td v-for="(item,id) of rownames" :key="id">
-            {{item}}
+          <td>
           </td>
         </tr>
-        <tr v-for="(row,id) of showData" :key="id">
-          <td v-for="(item,index) of row" :key="index" >
-            {{item}}
+        <tr>
+          <td>
           </td>
         </tr>
       </table><br>
       <div id="train-wrapper">
-        <button @click="svmtrain">SVM</button> |
-        <button>Bayes</button> |
-        <button @click="knntrain">KNN</button> |
-        <button>Logistic Regression</button> |
-        <button @click="RFtrain">Random Forest</button> |
+        <button @click="SvmTrain">SVM</button> |
+        <button @click="BayeTrain">Bayes</button> |
+        <button @click="KnnTrain">KNN</button> |
+        <button @click="LogTrain">Logistic Regression</button> |
+        <button @click="RFTrain">Random Forest</button> |
       </div>
     </div>
   </div>
@@ -27,53 +25,22 @@
 <script>
 import leaf from '../common/leaf'
 import KNN from 'ml-knn'
+import SVM from 'libsvm-js/asm'
+import Bayes from 'ml-naivebayes'
+import Matrix from 'ml-matrix'
+import LogisticRegression from 'ml-logistic-regression'
 import { RandomForestClassifier as RFClassifier } from 'ml-random-forest'
-const SVM = require('libsvm-js/asm')
+
 export default {
   data () {
     return {
-      trainData: [],
-      testData: [],
-      trainLabel: [],
-      testLabel: [],
-      showData: [],
-      rownames: [],
-      Data: [],
-      label: []
+      trainingSetX: [],
+      trainingSetY: [],
+      testingSetX: [],
+      testingSetY: []
     }
   },
   methods: {
-    svmtrain () {
-      const options = {
-        kernel: SVM.KERNEL_TYPES.POLYNOMIAL,
-        degree: 3
-      }
-      const svm = new SVM(options)
-      svm.train(this.Data, this.label)
-      const result = svm.predict(this.trainData)
-      console.log(result)
-    },
-    knntrain () {
-      const dataset = this.trainData
-      const predictions = this.trainLabel
-      const knn = new KNN(dataset, predictions, { k: 5 })
-      console.log(knn)
-    },
-    RFtrain () {
-      const options = {
-        seed: 3,
-        maxFeatures: 0.8,
-        replacement: true,
-        nEstimators: 25
-      }
-      var train = this.translateArray(this.trainData)
-      var test = this.translateArray(this.trainLabel)
-      const classifier = new RFClassifier(options)
-      classifier.train(train, test)
-      const result = classifier.predict(train)
-      console.log(result)
-    },
-
     translateArray (data) {
       var arr = []
       for (var i in data) {
@@ -83,37 +50,73 @@ export default {
         }
       }
       return arr
+    },
+    shuffleArray (array) {
+      for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1))
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
+      return array
+    },
+    KnnTrain () {
+      const knn = new KNN(this.trainingSetX, this.trainingSetY, { k: 5 })
+      const result = knn.predict(this.testingSetX)
+      console.log(result)
+      console.log(this.testingSetY)
+    },
+    SvmTrain () {
+      const options = {
+        kernel: SVM.KERNEL_TYPES.POLYNOMIAL,
+        degree: 3
+      }
+      const svm = new SVM(options)
+      svm.train(this.trainingSetX, this.trainingSetY)
+      const result = svm.predict(this.testingSetX)
+      console.log(result)
+      console.log(this.testingSetY)
+    },
+    BayeTrain () {
+      const bayes = new Bayes()
+      bayes.train(this.trainingSetX, this.trainingSetY)
+      const result = bayes.predict(this.testingSetX)
+      console.log(result)
+    },
+    LogTrain () {
+      const logreg = new LogisticRegression({ numSteps: 5000, learningRate: 5e-3 })
+      logreg.train(this.trainingSetY, this.trainingSetY)
+      const preValue = new Matrix(this.testingSetX)
+      const result = logreg.predict(preValue)
+      console.log(result)
+    },
+    RFTrain () {
+      const options = {
+        seed: 3,
+        maxFeatures: 0.8,
+        replacement: true,
+        nEstimators: 25
+      }
+      const classifier = new RFClassifier(options)
+      classifier.train(this.trainingSetX, this.trainingSetY)
+      const result = classifier.predict(this.testingSetX)
+      console.log(result)
     }
-
   },
   mounted: function () {
-    for (let i = 1; i < leaf.length; i++) {
-      const random = Math.floor(Math.random() * (i + 1));
-      [leaf[i], leaf[random]] = [leaf[random], leaf[i]]
-    }
-    const LeafClass = leaf.map(item => {
-      return item.type
+    const data = this.translateArray(this.shuffleArray(leaf))
+    const y = data.map(item => {
+      return item[1]
     })
-    const LeafData = leaf.map(item => {
-      delete item.type
-      delete item.specimenNumber
-      return item
+    const X = data.map(item => {
+      return item.slice(2, 16)
     })
-    const TrainLength = LeafData.length * 0.8
-    this.trainData = LeafData.slice(0, TrainLength)
-    this.testData = LeafData.slice(TrainLength, LeafData.length)
-    this.trainLabel = LeafClass.slice(0, TrainLength)
-    this.testLabel = LeafClass.slice(TrainLength, LeafData.length)
-    this.showData = leaf.slice(0, 20)
-    this.rownames = Object.keys(this.showData[0])
-    console.log(this.trainLabel)
-    const sum1 = this.trainLabel.reduce(function (prev, next) {
-      prev[next] = prev[next] + 1 || 1
-      return prev
-    }, {})
-    console.log(sum1)
-    const points = []
-    for (let i = 1; i < 37; i++) { points.push(i) }
+    const seperationSize = 0.9 * X.length
+    this.trainingSetX = X.slice(0, seperationSize)
+    this.trainingSetY = y.slice(0, seperationSize)
+    this.testingSetX = X.slice(seperationSize)
+    this.testingSetY = y.slice(seperationSize)
+    console.log(new Matrix(this.testingSetX))
   }
 }
 </script>
